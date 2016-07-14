18602525b365fbfa356d5dcf0d50657f7125f06f
derek_robertson <dwilliamrobertson@gmail.com>
2016-07-14 11:28:47 -0700

    changed time.rb

--- lib/tasks/modules/time.rb
+++ lib/tasks/modules/time.rb
@@ -1,5 +1,14 @@
 require 'tweetstream'
 
+@array = []
+
 @daemon.sample do |s|
-  puts "#{s.text}"
+  puts "#{s.user.location}"
+
+  if @array.length < 1000
+    @array.push(s.user.location)
+  else
+    puts "done"
+  end
 end
+
