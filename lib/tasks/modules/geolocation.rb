def80b2562009745527c4cf1468259b9a02f4476
Travis Smith <smithtm90@gmail.com>
2016-07-14 18:54:01 -0700

    working on geolocation.rb

---
+++ lib/tasks/modules/geolocation.rb
@@ -0,0 +1,13 @@
+require 'tweetstream'
+
+@daemon.sample do |s|
+  if (s.user.location =~ /USA/)
+    puts "---------------------"    
+    puts s.user.location
+    if (s.text =~ /(\d{2})/)
+      puts ""
+    end
+  end
+end
+
+#/,\s?[A-Z][a-z]{2,}/
